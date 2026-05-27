ab0486f8935d233adf6926614a4a72f2bfb5b332
TurboJax <turbojax07@gmail.com>
2026-05-27 16:08:24 -0400

    Converting the old LPC class to LPCPlugin.java

---
+++ bukkit/src/main/java/de/ayont/lpc/LPCPlugin.java
@@ -0,0 +1,33 @@
+package de.ayont.lpc;
+
+import de.ayont.lpc.api.LPC;
+import de.ayont.lpc.commands.LPCCommand;
+import de.ayont.lpc.bukkit.BukkitChatListener;
+import de.ayont.lpc.paper.PaperChatListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LPCPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        registerCommand();
+        saveDefaultConfig();
+        registerListeners();
+    }
+
+    public void registerCommand() {
+        String commandName = "lpc";
+        LPCCommand lpcCommand = new LPCCommand(this);
+
+        this.getCommand(commandName).setExecutor(lpcCommand);
+        this.getCommand(commandName).setTabCompleter(lpcCommand);
+    }
+
+    public void registerListeners() {
+        if (LPC.isPaper()) {
+            Bukkit.getPluginManager().registerEvents(new PaperChatListener(this), this);
+        } else {
+            Bukkit.getPluginManager().registerEvents(new BukkitChatListener(this), this);
+        }
+    }
+}
